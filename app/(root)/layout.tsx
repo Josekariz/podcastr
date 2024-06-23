6475ebb5333ed2a88f2b73aa7f88caf8dce7bd69
Jos <sejokarizz@gmail.com>
2024-06-23 19:37:03 +0300

    style page to be responsive for left side bar and right

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -6,11 +8,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
         <LeftSidebar />
-        {children}
-        <p className="text-white-1">Right Sidebar</p>
+        <section>
+          <div>
+            <div>
+              {/* <Image /> */}
+              MobileNav
+            </div>
+            <div>
+              Toaster
+              {children}
+            </div>
+          </div>
+        </section>
+        <RightSidebar />
       </main>
     </div>
   );
